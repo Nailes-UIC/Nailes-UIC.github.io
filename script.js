5d30d8941604a5f911133b160f1d560d339f196f
Stephen <snailes_230000001146@uic.edu.ph>
2024-10-19 16:43:29 +0800

    recent

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-// Dark Mode Toggle
+
 const toggleButton = document.getElementById('dark-mode-toggle');
 const body = document.body;
 
-// Check if Dark Mode was previously enabled (using localStorage)
 if (localStorage.getItem('dark-mode') === 'enabled') {
     body.classList.add('dark-mode');
 }
 
-// Add an event listener for toggle button
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
 
-    // Save the current mode to localStorage
+    
     if (body.classList.contains('dark-mode')) {
         localStorage.setItem('dark-mode', 'enabled');
     } else {
         localStorage.setItem('dark-mode', 'disabled');
     }
+});
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+navLinks.classList.toggle('nav-active');
+        
+hamburger.classList.toggle('toggle');
 });
